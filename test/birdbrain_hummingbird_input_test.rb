362805a7b3fed91df14b241915e2ab21484c5483
fmorton <fmorton@base2inc.com>
2021-01-30 16:49:03 -0500

    add birdbrain_device support

--- test/birdbrain_hummingbird_input_test.rb
+++ test/birdbrain_hummingbird_input_test.rb
@@ -8,23 +8,23 @@ class BirdbrainInputTest < BirdbrainTestSetup
     assert_true_or_false @hummingbird.hummingbird?
   end
 
-  def test_input_light
+  def test_input_hummingbird_light
     assert @hummingbird.light(1)
   end
 
-  def test_input_sound
+  def test_input_hummingbird_sound
     assert @hummingbird.sound(1)
   end
 
-  def test_input_distance
+  def test_input_hummingbird_distance
     assert @hummingbird.distance(1)
   end
 
-  def test_input_dial
+  def test_input_hummingbird_dial
     assert @hummingbird.dial(1)
   end
 
-  def test_input_voltage
+  def test_input_hummingbird_voltage
     assert @hummingbird.voltage(1)
   end
 end
