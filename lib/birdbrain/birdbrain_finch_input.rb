1ba776052538647dffd30e36c8bfe83c1479514d
fmorton <fmorton@base2inc.com>
2021-02-02 16:49:38 -0500

    refactor finch_input.sensor

--- lib/birdbrain/birdbrain_finch_input.rb
+++ lib/birdbrain/birdbrain_finch_input.rb
@@ -25,7 +25,12 @@ class BirdbrainFinchInput < BirdbrainRequest
   end
 
   def self.distance(device)
-    sensor(device, 'Distance', 'static', 0.0919, DEFAULT_UNLIMITED_MIN_RESPONSE, DEFAULT_UNLIMITED_MAX_RESPONSE)
+    distance_options = {}
+    distance_options[:factor] = 0.0919
+    distance_options[:min_response] = DEFAULT_UNLIMITED_MIN_RESPONSE
+    distance_options[:max_response] = DEFAULT_UNLIMITED_MAX_RESPONSE
+
+    sensor(device, 'Distance', 'static', distance_options)
   end
 
   def self.line(device, direction)
@@ -36,20 +41,29 @@ class BirdbrainFinchInput < BirdbrainRequest
   end
 
   def self.encoder(device, direction)
+    encoder_options = {}
+    encoder_options[:min_response] = DEFAULT_UNLIMITED_MIN_RESPONSE
+    encoder_options[:max_response] = DEFAULT_UNLIMITED_MAX_RESPONSE
+    encoder_options[:type_method] = 'to_f'
+
     calc_direction = 'Left' if direction == BirdbrainFinch::LEFT
     calc_direction = 'Right' if direction == BirdbrainFinch::RIGHT
 
-    sensor(device, 'Encoder', calc_direction, DEFAULT_FACTOR, DEFAULT_UNLIMITED_MIN_RESPONSE, DEFAULT_UNLIMITED_MAX_RESPONSE, 'to_f')
+    sensor(device, 'Encoder', calc_direction, encoder_options)
   end
 
-  def self.sensor(device, sensor, other = nil, factor = DEFAULT_FACTOR, min_response = DEFAULT_MIN_RESPONSE, max_response = DEFAULT_MAX_RESPONSE, type_method = DEFAULT_TYPE_METHOD)
+  def self.sensor(device, sensor, other = nil, options = {})
+    factor = options.key?(:factor) ? options[:factor] : DEFAULT_FACTOR
+    min_response = options.key?(:min_response) ? options[:min_response] : DEFAULT_MIN_RESPONSE
+    max_response = options.key?(:max_response) ? options[:max_response] : DEFAULT_MAX_RESPONSE
+    type_method = options.key?(:type_method) ? options[:type_method] : DEFAULT_TYPE_METHOD
+
     request = ['hummingbird', 'in', sensor]
     request << other unless other.nil?
     request << device
 
     response = response_body(request)
 
-    puts "DEBUG: response ================================================================================ #{response.inspect}"
     (response.nil? ? nil : bounds((response.to_f * factor).send(type_method), min_response, max_response))
   end
 end
